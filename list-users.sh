58a36105cfaeb60f249c3601856e92537828b11f
yesh01b <yeswanthb40@gmail.com>
2025-03-17 18:49:36 +0530

    updated

--- list-users.sh
+++ list-users.sh
@@ -1,41 +1,42 @@
 #!/bin/bash
 
-## GitHub API URL
+# GitHub API URL
 API_URL="https://api.github.com"
 
-## GitHub username and personal access token
+# GitHub username and personal access token
 USERNAME=$username
 TOKEN=$token
 
-## user and repository information
+# User and Repository information
 REPO_OWNER=$1
 REPO_NAME=$2
 
-## Function to make a GET request to the github API
+# Function to make a GET request to the GitHub API
 function github_api_get {
-    local endpoint=$1
+    local endpoint="$1"
     local url="${API_URL}/${endpoint}"
 
-    # send a GET request to GitHub API with Authentication
-    curl -s -u "${USERNAME}:${TOKEN}" "url"
+    # Send a GET request to the GitHub API with authentication
+    curl -s -u "${USERNAME}:${TOKEN}" "$url"
 }
 
-## Function to list users with read access to the repository
+# Function to list users with read access to the repository
 function list_users_with_read_access {
     local endpoint="repos/${REPO_OWNER}/${REPO_NAME}/collaborators"
 
     # Fetch the list of collaborators on the repository
-    collaborators="$(github_api_get "$endpoint" | jq -r '.[] | select (.permissions.pull == true) | .login')"
+    collaborators="$(github_api_get "$endpoint" | jq -r '.[] | select(.permissions.pull == true) | .login')"
 
     # Display the list of collaborators with read access
-    if [[-z "$collaborators"]]; then
-        echo "No users with read access found for ${REPO_OWNER}/${REPO_NAME}"
+    if [[ -z "$collaborators" ]]; then
+        echo "No users with read access found for ${REPO_OWNER}/${REPO_NAME}."
     else
         echo "Users with read access to ${REPO_OWNER}/${REPO_NAME}:"
-        echo $collaborators
+        echo "$collaborators"
     fi
 }
 
-## Main Script
-echo "Listing users with read access to ${REPO_OWNER}/${REPO_NAME}...."
+# Main script
+
+echo "Listing users with read access to ${REPO_OWNER}/${REPO_NAME}..."
 list_users_with_read_access
